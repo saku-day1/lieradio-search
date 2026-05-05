fdf5f115817838809ed667b5bde9560ec80e8cac
sakutarou <262035451+saku-day1@users.noreply.github.com>
2026-05-06 00:02:48 +0900

    Initial commit

---
+++ src/main/java/com/saku/lieradio/LieradioApplication.java
@@ -0,0 +1,13 @@
+package com.saku.lieradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieradioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LieradioApplication.class, args);
+    }
+
+}
